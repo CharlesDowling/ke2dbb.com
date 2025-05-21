e687271c477f7fdbb6d08edc4fe04292602512d1
CharlesDowling <charlesedwarddowlingjr@gmail.com>
2025-05-21 10:05:12 -0400

    Anchor Generation Script
    Created new function to load the JSON objects as anchors and automatically display.

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,13 @@
 export function elementGen(elementType, parent, ident, class_name="",  textContent="",  href="", src="", alt=""){
     //function elementGen (ElementType, Parent, Id, Class, TextContent, href, src, alt)
+    //ElementType : Type of HTML element to generate
+    //Parent : Parent object to appendChild()
+    //Ident : Element ID
+    //Class : Optional element class
+    //TextContent : textContent for object
+    //href : Link
+    //src : File source
+    //alt : Alternative text for images 
     let new_element = document.createElement(elementType);
     new_element.id = ident;
     new_element.className = class_name;
@@ -10,4 +18,24 @@ export function elementGen(elementType, parent, ident, class_name="",  textConte
         new_element.setAttribute("alt", alt);
     }
     document.getElementById(parent).appendChild(new_element);
+}
+
+export async function jsonElementLinkGen(type, parent, source){
+    //function jsonElementLinkGen (Type, Parent, Source)
+    //Type : Type of HTML element to generate
+    //Parent : Parent object
+    //Source : URL link for the json file
+    fetch(source)
+    .then((response) => response.json())
+    .then((json) => {
+        for (let x in Object.keys(json.links)){
+            console.log(json.links[x].id);
+            console.log(json.links[x].href);
+            console.log(json.links[x].textContent);
+            let id = json.links[x].id;
+            let href = json.links[x].href;
+            let textContent = json.links[x].textContent;
+            elementGen(type, parent,id,"",textContent,href);
+        }
+    });
 }
--- javascript/software_script.js
+++ javascript/software_script.js
@@ -1,4 +1,5 @@
 import { elementGen } from "/javascript/script.js";
+import { jsonElementLinkGen } from "/javascript/script.js";
 
 //function elementGen (ElementType, Parent, Id, Class, TextContent, href, src, alt)
 
@@ -8,23 +9,17 @@ import { elementGen } from "/javascript/script.js";
 //Software
 elementGen("div","body","top_body_div");
 elementGen("div","body","top_body_manuals");
+elementGen("h2","top_body_manuals","manuals_header","","Manuals & Books");
 elementGen("div","body","top_body_tutorial");
+elementGen("h2","top_body_tutorial","tutorial_header","","Tutorials & Guides");
 elementGen("div","body","top_body_software");
-
-
-elementGen("h1","top_body_div","wip","","WORK IN PROGRESS");
+elementGen("h2","top_body_software","software_header","","Software");
 
 //Bottom single box
 //Personal Software
 elementGen("div","body","bottom_body_div");
 elementGen("div","body","bottom_body_self");
 
-let host = window.location.hostname;
-
-let json_links
-
-fetch('/software/tutorial_links.json')
-    .then((response) => response.json())
-    .then((json) => lson_links = json);
-
-console.log(json);
+jsonElementLinkGen("a","top_body_tutorial","/software/tutorial_links.json");
+jsonElementLinkGen("a","top_body_manuals","/software/manuals_links.json");
+jsonElementLinkGen("a","top_body_software","/software/software_links.json");
